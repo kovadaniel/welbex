9d57bd95562fa5d29e194865b00cdf5b736df96b
kovadaniel <danielkovalenko50@gmail.com>
2023-07-21 04:05:47 +0300

    fixed some errors

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,7 +2,7 @@ function Contacts({contacts}) {
     return (
         <ul className="contact-refs">
             {contacts.map((contact, i) => 
-                <li key={contact.title} className={contact.class + ' contact-ref'}>
+                <li key={i} className={contact.class + ' contact-ref'}>
                     <a href={contact.ref}>
                         {contact.icon 
                         ? <img src={contact.icon} alt={contact.description}/>
